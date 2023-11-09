399e1ae5c4f038f1f484cd56a1bced4944bdef19
OxO <diegobae87@gmail.com>
2023-11-09 17:28:57 -0400

    subida reglas fasst

--- public/js/colores.js
+++ public/js/colores.js
@@ -1,11 +1,11 @@
 const coloresArcoiris = [ 
-    "#FF0000", // Rojo
-    "#FFA500", // Naranja
-    "#FFFF00", // Amarillo
+    "#ff0000", // Rojo
+    "#ffa500", // Naranja
+    "#ffff00", // Amarillo
     "#008000", // Verde
-    "#0000FF", // Azul
-    "#4B0082", // Índigo
-    "#800080"  // Violeta
+    "#0000ff", // Azul
+    "#4b0082", // Índigo
+    "#800080"  // Violeta*/
 ];
 const randomBackgroundColor = coloresArcoiris[Math.floor(Math.random() * coloresArcoiris.length)];
 document.body.style.backgroundColor = randomBackgroundColor;
--- public/js/obtenerDatos.js
+++ public/js/obtenerDatos.js
@@ -23,7 +23,7 @@ document.getElementById("like").addEventListener("click", function () {
   const bodyBackgroundColorHex = rgbToHex(bodyBackgroundColor);
   const jokeTextColor = window.getComputedStyle(document.getElementById("joke")).getPropertyValue("color");
   const jokeTextColorHex = rgbToHex(jokeTextColor);
-  const jokeText = document.getElementById("joke").textContent;
+  const jokeText = document.getElementById("joke").textContent;  
   const datosUsuario = obtenerDatosUsuario();
   const estadoAnimo = obtenerEstadoAnimo(datosUsuario);
   const personalidad = obtenerHipotesis(animalSeleccionado);
@@ -63,7 +63,7 @@ document.getElementById("dislike").addEventListener("click", function () {
   const bodyBackgroundColorHex = rgbToHex(bodyBackgroundColor);
   const jokeTextColor = window.getComputedStyle(document.getElementById("joke")).getPropertyValue("color");
   const jokeTextColorHex = rgbToHex(jokeTextColor);
-  const jokeText = document.getElementById("joke").textContent;
+  const jokeText = document.getElementById("joke").textContent;    
   const datosUsuario = obtenerDatosUsuario();
   const estadoAnimo = obtenerEstadoAnimo(datosUsuario);
   const personalidad = obtenerHipotesis(animalSeleccionado);
@@ -106,43 +106,122 @@ function rgbToHex(rgb) {
   });
   return "#" + hexValues.join("");
 }
-
+//Obtener datos
+function obtenerDatosUsuario() {
+  const bodyBackgroundColor = window.getComputedStyle(document.body).getPropertyValue("background-color");
+  const bodyBackgroundColorHex = rgbToHex(bodyBackgroundColor);
+  const jokeTextColor = window.getComputedStyle(document.getElementById("joke")).getPropertyValue("color");
+  const jokeTextColorHex = rgbToHex(jokeTextColor);
+  const animalPreferido = document.getElementById("perro").classList.contains("seleccionado") ? "perro" : "gato";
+  return {
+    colorFondo: bodyBackgroundColorHex,
+    colorTexto: jokeTextColorHex,
+    animalPreferido: animalPreferido
+  };
+}
 //
 /*
     Reglas del estado de animo
 */
 //
-function obtenerDatosUsuario() {
-    const bodyBackgroundColor = window.getComputedStyle(document.body).getPropertyValue("background-color");
-    const bodyBackgroundColorHex = rgbToHex(bodyBackgroundColor);
-    const jokeTextColor = window.getComputedStyle(document.getElementById("joke")).getPropertyValue("color");
-    const jokeTextColorHex = rgbToHex(jokeTextColor);
-    const animalPreferido = document.getElementById("perro").classList.contains("seleccionado") ? "perro" : "gato";
-    return {
-      colorFondo: bodyBackgroundColorHex,
-      colorTexto: jokeTextColorHex,
-      animalPreferido: animalPreferido
-    };
-  }
   
   function obtenerEstadoAnimo(datosUsuario) {
-    const rojo = "Atentos";
+    const rojo = "Atentos"; //Atentos
     const azul = "Calmados";
     const verde = "Ansioso";
     const amarillo = "Creativos";
     const naranja = "Concentrados";
-    const Violeta = "Relajados";
+    const violeta = "Relajados";
     const indigo = "Introspectivo";
-    const coloresAlegres = ["red", "orange", "yellow"];
-    const coloresTristes = ["blue", "indigo", "violet"];
-    const esAlegre = coloresAlegres.includes(datosUsuario.colorFondo) && datosUsuario.colorTexto === "#008000" && datosUsuario.animalPreferido === "perro";
-    const esTriste = coloresTristes.includes(datosUsuario.colorFondo) && datosUsuario.colorTexto === "#FFFFFF" && datosUsuario.animalPreferido === "gato";
-    if (esAlegre) {
-      return "alegre";
-    } else if (esTriste) {
-      return "triste";
+    //combinacionesDeColores
+    // Rojo +
+    const ROJO_AZUL = "Contraste entre emoción y serenidad";
+    const ROJO_NARANJA = "Energía apasionada";
+    const ROJO_AMARILLO = "Calidez y felicidad";
+    const ROJO_VERDE = "Contraste entre energía y calma";
+    const ROJO_INDIGO = "Pasión profunda y misterio";
+    const ROJO_VIOLETA = "Creatividad intensa y sofisticación"
+    // Naranja
+    const NARANJA_AMARILLO = "Alegría y vitalidad";
+    const NARANJA_VERDE = "Energía refrescante y creatividad";
+    const NARANJA_AZUL = "Contraste audaz entre vitalidad y serenidad";
+    const NARANJA_INDIGO = "Energía espiritual y creatividad profunda";
+    const NARANJA_VIOLETA = "Creatividad vibrante y toque misterioso";
+    //Amarillo +
+    const AMARILLO_VERDE = "Claridad mental, optimismo y serenidad";
+    const AMARILLO_AZUL = "Espiritualidad natural y equilibrio";
+    const AMARILLO_INDIGO = "Conexión espiritual y optimismo profundo";
+    const AMARILLO_VIOLETA = "Creatividad elevada y optimismo profundo";
+    // Verde +
+    const VERDE_AZUL = "Calma, equilibrio y naturaleza";
+    const VERDE_INDIGO = "Espiritualidad natural y equilibrio";
+    const VERDE_VIOLETA = "Creatividad elevada y optimismo profundo";
+    // Azul +
+    const AZUL_INDIGO = "Profundidad emocional y serenidad";
+    const AZUL_VIOLETA = "Inspiración y creatividad tranquila";
+    // Indigo +
+    const INDIGO_VIOLETA = "Misterio espiritual y introspección";
+    // Tomar cosas
+    colorBody = datosUsuario.colorFondo; //color fondo
+    colorTexto = datosUsuario.colorTexto; //color texto
+    if (colorBody == "#ff0000" && colorTexto == "#ff0000") { // Rojo + Rojo
+      return rojo;
+    } else if (colorBody == "#0000ff" && colorTexto == "#0000ff") { // azul + azul
+      return azul;
+    } else if (colorBody == "#0000ff" && colorTexto == "#008000"){ // verde + verde
+      return verde; 
+    } else if (colorBody == "#0000ff" && colorTexto == "#ffff00"){ // amarillo + amarillo
+      return amarillo;
+    } else if (colorBody == "#0000ff" && colorTexto == "#ffa500"){ // naranja + naranja
+      return naranja;
+    } else if (colorBody == "#0000ff" && colorTexto == "#800080"){ // violeta + violeta
+      return violeta;
+    } else if (colorBody == "#0000ff" && colorTexto == "#4b0082"){ // indigo + indigo
+      return indigo;
+    } else if (colorBody == "#ff0000" && colorTexto == "#0000ff" || colorTexto == "#ff0000" && colorBody == "#0000ff"){ // Rojo + Azul o Azul + Rojo
+      return ROJO_AZUL;
+    } else if (colorBody == "#ff0000" && colorTexto == "#ffa500" || colorTexto == "#ff0000" && colorBody == "#ffa500"){ // Rojo + Naranja
+      return ROJO_NARANJA;
+    } else if (colorBody == "#ff0000" && colorTexto == "#ffff00" || colorTexto == "#ff0000" && colorBody == "#ffff00"){ // Rojo + Amarillo
+      return ROJO_AMARILLO;
+    } else if (colorBody == "#ff0000" && colorTexto == "#008000" || colorTexto == "#ff0000" && colorBody == "#008000"){ // Rojo + Verde
+      return ROJO_VERDE;
+    } else if (colorBody == "#ff0000" && colorTexto == "#4b0082" || colorTexto == "#ff0000" && colorBody == "#4b0082"){ // Rojo + Verde
+      return ROJO_INDIGO;
+    } else if (colorBody == "#ff0000" && colorTexto == "#800080" || colorTexto == "#ff0000" && colorBody == "#800080"){ // Rojo + Verde
+      return ROJO_VIOLETA;
+    } else if (colorBody == "#ffa500" && colorTexto == "#ffff00" || colorTexto == "#ffa500" && colorBody == "#ffff00"){ // Naranja + Amarillo
+      return NARANJA_AMARILLO;
+    } else if (colorBody == "#ffa500" && colorTexto == "#008000" || colorTexto == "#ffa500" && colorBody == "#008000"){ // Naranja + Verde
+      return NARANJA_VERDE;
+    } else if (colorBody == "#ffa500" && colorTexto == "#0000ff" || colorTexto == "#ffa500" && colorBody == "#0000ff"){ // Naranja + Azul
+      return NARANJA_AZUL;
+    } else if (colorBody == "#ffa500" && colorTexto == "#4b0082" || colorTexto == "#ffa500" && colorBody == "#4b0082"){ // Naranja + Indigo
+      return NARANJA_INDIGO;
+    } else if (colorBody == "#ffa500" && colorTexto == "#800080" || colorTexto == "#ffa500" && colorBody == "#800080"){ // Naranja + Violeta
+      return NARANJA_VIOLETA;
+    } else if (colorBody == "#ffff00" && colorTexto == "#008000" || colorTexto == "#ffff00" && colorBody == "#008000"){ // Amarillo + Verde
+      return AMARILLO_VERDE;
+    } else if (colorBody == "#ffff00" && colorTexto == "#0000ff" || colorTexto == "#ffff00" && colorBody == "#0000ff"){ // Amarillo + Azul
+      return AMARILLO_AZUL;
+    } else if (colorBody == "#ffff00" && colorTexto == "#4b0082" || colorTexto == "#ffff00" && colorBody == "#4b0082"){ // Amarillo + Indigo
+      return AMARILLO_INDIGO;
+    } else if (colorBody == "#ffff00" && colorTexto == "#800080" || colorTexto == "#ffff00" && colorBody == "#800080"){ // Amarillo + Violeta
+      return AMARILLO_VIOLETA;
+    } else if (colorBody == "#008000" && colorTexto == "#0000ff" || colorTexto == "#008000" && colorBody == "#0000ff"){ // Verde + Azul
+      return VERDE_AZUL;
+    } else if (colorBody == "#008000" && colorTexto == "#4b0082" || colorTexto == "#008000" && colorBody == "#4b0082"){ // Verde + Indigo
+      return VERDE_INDIGO;
+    } else if (colorBody == "#008000" && colorTexto == "#800080" || colorTexto == "#008000" && colorBody == "#800080"){ // Verde + Violeta
+      return VERDE_VIOLETA;
+    } else if (colorBody == "#0000ff" && colorTexto == "#4b0082" || colorTexto == "#0000ff" && colorBody == "#4b0082"){ // Azul + Indigo
+      return AZUL_INDIGO;
+    } else if (colorBody == "#0000ff" && colorTexto == "#800080" || colorTexto == "#0000ff" && colorBody == "#800080"){ // Azul + Violeta
+      return AZUL_VIOLETA;
+    } else if (colorBody == "#4b0082" && colorTexto == "#800080" || colorTexto == "#4b0082" && colorBody == "#800080"){ // Indigo + Violeta
+      return INDIGO_VIOLETA;
     } else {
-      return "neutral";
+      return "neutro";
     }
   }
 
